8dd6f073e1a7451ed7871fa8b8261de352b5fa3a
Christoph Grossbaier <cg@rivutec.com>
2016-01-04 23:49:10 +0100

    update

--- geomap/management/commands/createsu.py
+++ geomap/management/commands/createsu.py
@@ -7,5 +7,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", "admin")
+            User.objects.create_superuser("admin", "admin@admin.com", "admin12345")
 
