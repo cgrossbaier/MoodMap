5bc6ec59876c4f66f9b83df7ed6ecc82b2f143e3
Christoph Grossbaier <cg@rivutec.com>
2016-01-05 10:52:34 +0100

    static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -164,5 +164,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "geomap", "static")
-STATIC_URL = '/static/'
+STATIC_URL = '/geomap/static/'
 
